eac0718e1025a7ed111896c63e9b69b4adc7a412
Syndee <Cynthiaineh02@gmail.com>
2020-07-16 04:57:49 +0100

    Edit profile

---
+++ lib/Pages/editProfile.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+class EditProfile extends StatefulWidget {
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  bool isSwitched = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit profile'),
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 15.0),
+            Container(
+              width: 300.0,
+              height: 70.0,
+              color: Colors.grey[50],
+              child: CircleAvatar(
+                child: Icon(
+                  Icons.person_outline,
+                  color: Colors.white,
+                ),
+                backgroundColor: Colors.grey,
+              ),
+            ),
+            Container(
+              width: 415.0,
+              margin: EdgeInsets.fromLTRB(0, 10.0, 0, 0),
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+              color: Colors.white,
+              child: Text(
+                'General',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+              child: Form(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.person,
+                          size: 20.0,
+                        ),
+                        labelText: 'FirstName:',
+                        labelStyle: TextStyle(fontSize: 15.0),
+                        hintText: '',
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.person,
+                            size: 20.0,
+                          ),
+                          labelText: 'LastName:',
+                          labelStyle: TextStyle(fontSize: 15.0),
+                          hintText: ''),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                            size: 20.0,
+                          ),
+                          labelText: 'Email:',
+                          labelStyle: TextStyle(fontSize: 15.0),
+                          hintText: ''),
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.phone,
+                            size: 20.0,
+                          ),
+                          labelText: 'Phone:',
+                          labelStyle: TextStyle(fontSize: 15.0),
+                          hintText: ''),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.location_city,
+                            size: 20.0,
+                          ),
+                          labelText: 'City,State:',
+                          labelStyle: TextStyle(fontSize: 15.0),
+                          hintText: ''),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.fromLTRB(21.0, 0, 30.0, 5.0),
+                  padding: EdgeInsets.symmetric(vertical: 5.0),
+                  child: Text(
+                    'Become a Service Provider',
+                    style: TextStyle(fontSize: 17.0),
+                  ),
+                ),
+                Container(
+                  child: Switch(
+                    value: isSwitched,
+                    onChanged: (value) {
+                      setState(() {
+                        isSwitched = value;
+                        print(isSwitched);
+                      });
+                    },
+                    activeTrackColor: Colors.blue[100],
+                    activeColor: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              child: ButtonTheme(
+                minWidth: 120.0,
+                height: 40.0,
+                child: RaisedButton(
+                  onPressed: () => {},
+                  color: Colors.blue,
+                  child: Text(
+                    'Save',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,5 +1,6 @@
 //import 'dart:ffi';
 
+import 'package:Orsul_v1/Pages/editProfile.dart';
 import 'package:Orsul_v1/Tabs/homeTab.dart';
 import 'package:Orsul_v1/Tabs/jobsTab.dart';
 import 'package:Orsul_v1/Tabs/messageTab.dart';
@@ -10,89 +11,84 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-
-
 class Home extends StatefulWidget {
-    //had to comment out the constructor.
+  //had to comment out the constructor.
   final FirebaseUser user;
-    //create a constructor so each user is created uniquely
-  const Home({
-    Key key, 
-      this.user
-    }) : super(key: key);
+  //create a constructor so each user is created uniquely
+  const Home({Key key, this.user}) : super(key: key);
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-    int bottomNavIndex = 0;
+  int bottomNavIndex = 0;
 
-    //the tabs to be displayed for each bottom nav button
-    final tabs =[
-      HomeTab(),
-      JobsTab(),
-      MessageTab(),
-      WalletTab()
-    ];
+  //the tabs to be displayed for each bottom nav button
+  final tabs = [HomeTab(), JobsTab(), MessageTab(), WalletTab()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Our professionals are at your service'),
-        
       ),
       body: tabs[bottomNavIndex],
-      
       drawer: Drawer(
-        child: ListView(
-          children:[
-            UserAccountsDrawerHeader(
-        accountName: Text("Joshua"), 
-        accountEmail: Text("adeniyioojo09@gmail.com"),
-        currentAccountPicture: GestureDetector(
-          child: CircleAvatar(
-            child: Icon(Icons.person_outline, color: Colors.white,),
-            backgroundColor: Colors.grey[400],
+        child: ListView(children: [
+          UserAccountsDrawerHeader(
+            accountName: Text("Joshua"),
+            accountEmail: Text("adeniyioojo09@gmail.com"),
+            currentAccountPicture: GestureDetector(
+              child: CircleAvatar(
+                child: Icon(
+                  Icons.person_outline,
+                  color: Colors.white,
+                ),
+                backgroundColor: Colors.grey[400],
+              ),
+              onTap: () {},
+            ),
           ),
-          onTap: (){},
-        ),
-        ),
-        //members of the drawer
-        InkWell(
-          child: ListTile(
-            title: Text('Edit Profile'),
-            leading: Icon(Icons.edit)
+          //members of the drawer
+          InkWell(
+            child: ListTile(
+              title: Text('Edit Profile'),
+              leading: Icon(Icons.edit),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => EditProfile()));
+              },
+            ),
           ),
-        ),
-        InkWell(
-          child: ListTile(
-            title: Text('History'),
-            leading: Icon(Icons.history)
+          InkWell(
+            child: ListTile(
+              title: Text('History'),
+              leading: Icon(Icons.history),
+              onTap: () {},
+            ),
           ),
-        ),
-        InkWell(
-          child: ListTile(
-            title: Text('Settings'),
-            leading: Icon(Icons.settings)
+          InkWell(
+            child: ListTile(
+              title: Text('Settings'),
+              leading: Icon(Icons.settings),
+              onTap: () {},
+            ),
           ),
-        ),
-        InkWell(
-          child: ListTile(
-            title: Text('Logout'),
-            leading: Icon(Icons.exit_to_app)
+          InkWell(
+            child: ListTile(
+              title: Text('Logout'),
+              leading: Icon(Icons.exit_to_app),
+              onTap: () {},
+            ),
           ),
-        ),
-          ]
-        ),
-
+        ]),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.blueGrey[300],
-        currentIndex: bottomNavIndex, 
-        selectedFontSize: 15.0,       
+        currentIndex: bottomNavIndex,
+        selectedFontSize: 15.0,
         items: [
           //home button
           BottomNavigationBarItem(
@@ -100,38 +96,37 @@ class _HomeState extends State<Home> {
             title: Text("Home"),
             activeIcon: Icon(Icons.home),
             backgroundColor: Colors.blue,
-            ),
-            //Jobs button
-            BottomNavigationBarItem(
+          ),
+          //Jobs button
+          BottomNavigationBarItem(
             icon: Icon(Icons.work),
             title: Text("Jobs"),
             activeIcon: Icon(Icons.work),
             backgroundColor: Colors.blue,
-            ),
-            //Message button
-            BottomNavigationBarItem(
+          ),
+          //Message button
+          BottomNavigationBarItem(
             icon: Icon(Icons.send),
             title: Text("Message"),
             activeIcon: Icon(Icons.send),
             backgroundColor: Colors.blue,
-            ),
-            //Wallet icon.
-            BottomNavigationBarItem(
+          ),
+          //Wallet icon.
+          BottomNavigationBarItem(
             icon: Icon(Icons.storage),
             title: Text("Wallet"),
-            activeIcon: Icon(Icons.account_balance_wallet
-            ),
+            activeIcon: Icon(Icons.account_balance_wallet),
             backgroundColor: Colors.blue,
-            ),
+          ),
         ],
-        onTap: (val){
+        onTap: (val) {
           setState(() {
             bottomNavIndex = val;
           });
-          },
-          unselectedItemColor: Colors.grey[100],
-          selectedItemColor: Colors.yellowAccent[700],
-        ) ,
+        },
+        unselectedItemColor: Colors.grey[100],
+        selectedItemColor: Colors.yellowAccent[700],
+      ),
     );
   }
 }
